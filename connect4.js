51c047700770244a9d4d07bd260dc26b410894d1
mo-enokida <mo.enokida@gmail.com>
2021-04-08 14:18:51 -0700

    horix,vert,diag win conditions

--- connect4.js
+++ connect4.js
@@ -154,9 +154,9 @@ function checkForWin() {
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDL = [[y, x], [y - 1, x - 1], [y - 2, x - 2], [y - 3, x - 3]];
+      let diagDR = [[y, x], [y + 1, x - 1], [y + 1, x - 2], [y + 1, x - 3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
